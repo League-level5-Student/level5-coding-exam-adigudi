9f6c40f31a111f4f7da7d73f5ec955ec2be03e18
Adi Gudi <30880671+adigudi@users.noreply.github.com>
2020-04-11 17:08:58 -0700

    Update CodingExamA.java

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -6,6 +6,7 @@ import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
 
+
 public class CodingExamA {
 	public static void main(String[] args) {
 		/*
@@ -21,6 +22,37 @@ public class CodingExamA {
 		 * 
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
-
+		String numR = JOptionPane.showInputDialog("How many Robots?");
+		int numRobots = Integer.parseInt(numR);
+		String colorS = JOptionPane.showInputDialog("What color do you want the shapes to be? (Red, Green, Blue)");
+		String numS = JOptionPane.showInputDialog("How many sides each shape will have? (>0)");
+		int sides = Integer.parseInt(numS);
+		
+		Thread[] robots = new Thread[numRobots];
+		for (int i = 0; i < robots.length; i++) {
+			int y = i;
+			robots[i] = new Thread (() -> {
+				Robot robot = new Robot();
+				robot.setSpeed(10);
+				robot.moveTo(50 + (150*y), 300);
+				robot.penDown();
+				if(colorS.equalsIgnoreCase("red")) {
+					robot.setPenColor(Color.RED);
+				}
+				else if(colorS.equalsIgnoreCase("green")){
+					robot.setPenColor(Color.GREEN);
+				}
+				else {
+					robot.setPenColor(Color.BLUE);
+				}
+			for (int j = 0; j < sides; j++) {
+				robot.move(50);
+				int degree = 360/sides;
+				robot.turn(degree);
+			}
+		});
+			robots[i].start();
 	}
 }
+}
+	
